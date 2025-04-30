d3920cfcd5253f6ae9f4a0bacab83bc031944cee
chmpgnesupernova <101654598+chmpgnesupernova@users.noreply.github.com>
2025-04-30 20:04:50 +0900

    first commit

---
+++ cppEx.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+class simpleRational {
+	int denominator = 0;
+	int nominator = 1;
+	double result = 0;
+
+public:
+	simpleRational(int denom = 0, int nom = 1) { result = denom / nom; }
+
+	void Print() const {
+		std::cout << result;
+	}
+};
+
+int main()
+{
+	simpleRational r1(100, 25);
+	r1.Print();
+}
+
