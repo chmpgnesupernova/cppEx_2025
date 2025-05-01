096fc9f7ba67ceab6b841d292423b163262241f1
chmpgnesupernova <101654598+chmpgnesupernova@users.noreply.github.com>
2025-05-01 19:53:45 +0900

    string stream EX

---
+++ 2025.05.01/stringstreamEX.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include<vector>
+
+int getVnumber(int& n, int& v) {
+	std::string tempN;
+	std::string tempArr;
+	std::string tempV;
+
+	std::getline(std::cin, tempN);
+	std::istringstream iss1{ tempN };
+
+	std::getline(std::cin, tempArr);
+	std::istringstream iss2{ tempArr };
+
+	std::getline(std::cin, tempV);
+	std::istringstream iss3{ tempV };
+
+	iss1 >> n;
+	iss3 >> v;
+
+	int temp;
+	int count = 0;
+	int* Arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		iss2 >> temp;
+		Arr[i] = temp;
+	}
+
+	for (int k = 0; k < n; k++) {
+		if (Arr[k] == v) count++;
+	}
+
+	delete[] Arr;
+
+	return count;
+}
+
+int main() {
+	int N = 0;
+	int V = 0;
+	std::cout << getVnumber(N, V);
+}
+
--- cppEx.cpp
+++ cppEx.cpp
@@ -3,7 +3,59 @@
 #include <sstream>
 #include<vector>
 
-int main(){
+int getVnumber(int& n, int& v) {
+	std::string tempN;
+	std::string tempArr;
+	std::string tempV;
+
+	std::getline(std::cin, tempN);
+	std::istringstream iss1{ tempN };
+
+	std::getline(std::cin, tempArr);
+	std::istringstream iss2{ tempArr };
+
+	std::getline(std::cin, tempV);
+	std::istringstream iss3{ tempV };
+
+	iss1 >> n;
+	iss3 >> v;
+
+	int temp;
+	int count = 0;
+	int* Arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		iss2 >> temp;
+		Arr[i] = temp;
+	}
+
+	for (int k = 0; k < n; k++) {
+		if (Arr[k] == v) count++;
+	}
+
+	delete[] Arr;
+
+	return count;
+
+	/*
+	std::cout << tempN << '\n';
+	std::cout << tempArr << '\n';
+	std::cout << tempV << '\n';
+
+	std::cout << n << '\n';
+	std::cout << v << '\n';
+*/
+}
+
+int main() {
+
+	int N = 0;
+	int V = 0;
+
+	std::cout << getVnumber(N, V);
+
+
+	/*
 	std::string line;
 	int token;
 	std::vector<int> v{};
@@ -17,5 +69,6 @@ int main(){
 	for (int k = 0; k < v.size(); k++) {
 		std::cout << v[k] << ' ';
 	}
+	*/
 }
 
