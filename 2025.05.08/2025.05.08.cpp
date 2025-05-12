a89acd9b3c3ddc6238747a052f26a7fdeea2dba5
chmpgnesupernova <101654598+chmpgnesupernova@users.noreply.github.com>
2025-05-12 21:52:19 +0900

    class ex

---
+++ 2025.05.08/2025.05.08.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream> 
+#include <string>
+#include <vector>
+#include <cmath>
+
+
+void isAscending() {
+	std::string state = "non";
+	int past_num;
+	int current_num;
+	std::cin >> past_num;
+	if (past_num == 1) {
+		state = "ascending";
+		while (std::cin >> current_num) {
+			if (current_num != past_num + 1) { state = "mixed"; break; }
+			past_num = current_num;
+		}
+	}
+	else if (past_num == 8) {
+		state = "descending";
+		while (std::cin >> current_num) {
+			if (current_num != past_num - 1) { state = "mixed"; break; }
+			past_num = current_num;
+		}
+	}
+	else state = "mixed";
+
+	std::cout << state;
+}
+
+void countRemain() {
+	std::vector<int> remain{};
+	int input_num;
+	bool approve = true;
+	while (std::cin >> input_num) {
+		for (const int& elem : remain) {
+			approve = (elem != input_num % 42) ? true : false;
+			if (!approve) break;
+		}
+		if (approve) remain.push_back(input_num % 42);
+	}
+	std::cout << remain.size();
+}
+
+void combo() {
+	std::string score;
+	int test = 0;
+	int add_score = 0;
+	int sum;
+	char past_ch;
+	char present_ch;
+	std::cin >> test;
+
+	for (int i = 0; i < test; i++) {
+		sum = 0;
+		add_score = 0;
+		std::cin >> score;
+		past_ch = score[0];
+		for (const char& elem : score) {
+			present_ch = elem;
+			if (past_ch == 'X') {
+				if (present_ch == 'O') { add_score = 1; sum += add_score; }
+				if (present_ch == 'X') { add_score = 0; }
+			}
+			if (past_ch == 'O') {
+				if (present_ch == 'O') { add_score++; sum += add_score; }
+				if (present_ch == 'X') { add_score = 0; }
+			}
+			past_ch = present_ch;
+		}
+		std::cout << sum << '\n';
+	}
+	
+}
+
+void posAlpha() {
+	std::string word;
+	int pos = -1;
+	std::cin >> word;
+	for (char alpha = 'a'; alpha <= 'z'; alpha++) {
+		for (int i = 0; i < word.size(); i++) {
+			if (word[i] == alpha) { pos = i; break; }
+		}
+		std::cout << pos << ' ';
+		pos = -1;
+	}
+}
+
+void isRightTri(int a, int b, int c) {
+	int largest = a, x1=b, x2=c;
+	if (b > largest) { largest = b; x1 = a; x2 = c; }
+	if (c > largest) { largest = c; x1 = a; x2 = b; }
+
+	
+	if (pow(largest, 2) == pow(x1, 2) + pow(x2, 2)) {
+		std::cout << "right" << '\n';
+	}
+	else std::cout << "wrong" << '\n';
+}
+
+void getNum() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	int num_part;
+	int T = 0;
+	int P = 0;
+	std::string size_info;
+	std::cin >> num_part;
+	std::getline(std::cin, size_info);
+	std::cin >> T >> P;
+	for (auto elem : size_info) {
+		int (elem) / T
+	}
+}
+
+int main() {
+	//countRemain();
+	//combo();
+	//posAlpha();
+	/*int a, b, c;
+	std::cin >> a >> b >> c;
+	while (!(a == 0 && b == 0 && c == 0)) {
+		isRightTri(a, b, c);
+		std::cin >> a >> b >> c;
+	}*/
+
+
+}
---
+++ 2025.05.12/2025.05.12.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+class BigUnsigned {
+    std::vector<int> b1{};
+    int sum = 0;
+    int carry = 0;
+    int elem = 0;
+public:
+    int getSize() {
+        return b1.size();
+    }
+    int getElem(int i) {
+        return b1[i];
+    }
+    void insertElem(int i) {
+        b1.insert(b1.begin(), i);
+    }
+    BigUnsigned(const unsigned int& x = 0) {
+        int input = x;
+        while (input > 0) {
+            b1.insert(b1.begin(), input % 10);
+            input = input / 10;
+        }
+    }
+    BigUnsigned(const std::string& x = "0") {
+        for (int i = 0; i < x.size(); i++) {
+            b1.insert(b1.begin(), (int)(x[i] - '0'));
+        }
+    }
+    BigUnsigned operator +(BigUnsigned& b2) {
+        BigUnsigned new_b(0);
+        int size = (b1.size() >= b2.getSize()) ? b1.size() : b2.getSize();
+        for (int i = size - 1; i >= 0; i--) {
+            sum = b1[i] + b2.getElem(i) + carry;
+            if (i == 0) { new_b.insertElem(sum); break; }
+            if (sum >= 10) { sum -= 10;      carry = 1; }
+            else { carry = 0; }
+
+            new_b.insertElem(sum);
+        }
+        return new_b;
+    }
+};
+
+std::ostream& operator <<(std::ostream& os, BigUnsigned& b1) {
+    for (int i = 0; i < b1.getSize(); i++) {
+        os << b1.getElem(i);
+    }
+    return os;
+}
+
+int main() {
+    BigUnsigned x1(99);
+    BigUnsigned x2("11");
+    BigUnsigned x3 = x1 + x2;
+    std::cout << x1 << ' ' << x2 << ' ' << x3;
+}
